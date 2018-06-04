9f6456a10a3a76876064d653f40059378e30c1c0
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-04 10:45:17 -0400

    finished rna transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,16 @@
 def to_rna(dna_strand):
-    pass
+    result = ''
+
+    for i in dna_strand:
+            if i == 'C':
+                result = result + 'G'
+            elif i == 'G':
+                result = result + 'C'
+            elif i == 'T':
+                result = result + 'A'
+            elif i == 'A':
+                result = result + 'U'
+            else:
+                raise ValueError
+
+    return result
