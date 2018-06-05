0428636663363e3bbc70a7ea30485d3cddc54a33
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-05 16:30:27 -0400

    finished python hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strand lengths are not equal!")
+
+    diff = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            diff += 1
+
+    return diff
