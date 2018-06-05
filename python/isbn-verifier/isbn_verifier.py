e4f992d25010b14717cd6d1541f74cf99cab1837
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-05 13:21:23 -0400

    finished isbn-verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,34 @@
 def verify(isbn):
-    pass
+    nums = []
+    
+    for i in isbn:
+        print(str(i))
+        try:
+            if i == '-':
+                continue
+            elif i == ('X' or 'x'):
+                if len(nums) != 9:
+                    return False
+                nums.append(10)
+            else:
+                nums.append(int(i))
+        except:
+            return False
+
+    if len(nums) != 10:
+        return False
+    
+    result = 0
+
+    for i in range(10):
+        result += nums[i] * (i + 1)
+        print("nums = " + str(nums[i]), end='')
+        print("; nums * i = " + str(nums[i] * (i + 1)), end='')
+        print("; i = " + str(i), end='')
+        print("; result = " + str(result))
+        
+    final = result % 11
+
+    print(str(final == 0))
+
+    return (result % 11 == 0)
