9ff0b8d26a0a5e02d7c4e938b0cfc62d0ce29e1a
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-06 17:11:07 -0400

    worked on bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,24 @@
+import re
+import string
+
 def hey(phrase):
-    pass
+    shouting = re.compile(r'^[A-Z\d,\t\n!\.%^*@#$(*^ ]+$')
+    question = re.compile(r'.\?$')
+
+    phrase = phrase.lstrip().rstrip()
+
+    is_question = question.match(phrase)
+
+    if phrase == "":
+        return "Fine. Be that way!"
+    
+    if shouting.match(phrase):
+        if is_question:
+            return "Calm down, I know what I'm doing!"
+        else:
+            return "Whoa, chill out!"
+    else:
+        if is_question:
+            return "Sure."
+
+    return "Whatever."
