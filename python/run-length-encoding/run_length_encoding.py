6b128f758c1706293af3cb19b48969e1d3aaaf3f
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-08 13:52:37 -0400

    finished run length encoding, fetched meetup

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,37 @@
 def decode(string):
-    pass
+    decoded = ''
+    num_as_str = ''
+
+    for i in range(len(string)):
+        if string[i].isdigit():
+            num_as_str = num_as_str + string[i]
+        else:
+            if num_as_str == '':
+                num_as_str = '1'
+            decoded = decoded + (string[i] * int(num_as_str))
+            num_as_str = ''
+        
+    return decoded
 
 
 def encode(string):
-    pass
+    encoded = ''
+    num = 0
+
+    for i in range(len(string)):
+        if (i == (len(string) - 1)):
+            if num == 0:
+                encoded = encoded + string[i]
+            else:
+                encoded = encoded + str(num + 1) + string[i]
+                num = 0
+        elif string[i] != string[i+1]:
+            if num == 0:
+                encoded = encoded + string[i]
+            else:
+                encoded = encoded + str(num + 1) + string[i]
+                num = 0
+        else:
+            num = num + 1
+
+    return encoded
