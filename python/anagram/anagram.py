b4a5f93364799c929c3781f94744060ddffc16e9
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-13 14:27:02 -0400

    finished diff of squares

---
+++ python/anagram/anagram.py
@@ -0,0 +1,2 @@
+def detect_anagrams(word, candidates):
+    pass
--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,22 @@
 def square_of_sum(count):
-    pass
+    the_sum = 0
 
+    for i in range(count + 1):
+        the_sum = the_sum + i
+
+    return the_sum * the_sum
 
 def sum_of_squares(count):
-    pass
+    the_sum = 0
 
+    for i in range(count + 1):
+        the_sum = the_sum + (i**2)
+
+    return the_sum
 
 def difference(count):
-    pass
+    sum_of_sq = sum_of_squares(count)
+
+    sq_of_sum = square_of_sum(count)
+
+    return sq_of_sum - sum_of_sq
