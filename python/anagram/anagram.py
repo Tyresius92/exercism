4d45c5d1480d16a166bd17e9bbab3b2383a227ff
Tyresius92 <tyrelclayton92@gmail.com>
2018-06-13 14:42:43 -0400

    finished anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,17 @@
 def detect_anagrams(word, candidates):
-    pass
+    anagrams = []
+
+    for potential in candidates:
+        if (len(potential) != len(word)) | (potential.lower() == word.lower()):
+            continue
+        
+        potential_copy = potential.lower()
+        
+        for i in word.lower():            
+            if i in potential_copy:
+                potential_copy = potential_copy.replace(i, '', 1)
+
+        if potential_copy == '':
+            anagrams.append(potential)
+
+    return anagrams
