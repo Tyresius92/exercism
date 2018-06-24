68f7f97fb7e9f24ce5841877cc4a56e03aa1f3b9
Tyrel <tyrelclayton92@gmail.com>
2018-06-24 16:32:57 -0400

    created src files, fixed compile errors

---
+++ c/gigasecond/src/gigasecond.c
@@ -0,0 +1,8 @@
+#include "gigasecond.h"
+
+time_t gigasecond_after(time_t date_time)
+{
+        (void) date_time; 
+
+        return date_time; 
+}
---
+++ c/gigasecond/src/gigasecond.h
@@ -0,0 +1,9 @@
+
+#ifndef GIGASECOND_H
+#define GIGASECOND_H
+
+#include <time.h>
+
+time_t gigasecond_after(time_t date_time); 
+
+#endif
