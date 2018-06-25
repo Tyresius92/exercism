aaaf259c3910cbf8650dc3abfe00d292020b7946
Tyrel <tyrelclayton92@gmail.com>
2018-06-24 23:22:50 -0400

    finished gigasecond

--- c/gigasecond/src/gigasecond.c
+++ c/gigasecond/src/gigasecond.c
@@ -1,8 +1,9 @@
 #include "gigasecond.h"
+#include <stdio.h>
+
+const int GIGASECOND = 1000000000;
 
 time_t gigasecond_after(time_t date_time)
 {
-        (void) date_time; 
-
-        return date_time; 
+        return date_time + GIGASECOND; 
 }
