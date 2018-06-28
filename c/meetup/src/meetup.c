11f51f165b784c94a1a8c4c010c470eb495a589c
Tyrel <tyrelclayton92@gmail.com>
2018-06-28 12:30:10 -0400

    got the tests running

---
+++ c/meetup/src/meetup.c
@@ -0,0 +1,13 @@
+#include "meetup.h"
+
+int meetup_day_of_month(int year, int month, char *which, char *day)
+{
+        (void) year; 
+        (void) month; 
+        (void) which; 
+        (void) day; 
+
+        
+
+        return 0; 
+}
---
+++ c/meetup/src/meetup.h
@@ -0,0 +1,9 @@
+#ifndef MEETUP_H
+#define MEETUP_H
+
+#include <stdlib.h>
+#include <time.h>
+
+int meetup_day_of_month(int year, int month, char *which, char *day); 
+
+#endif
