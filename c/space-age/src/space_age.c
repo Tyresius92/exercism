c59d9f293aaf64ef0ad82c2c33ceca0c58e3267c
Tyrel <tyrelclayton92@gmail.com>
2018-06-27 22:31:07 -0400

    swapped out the consts for defines

--- c/space-age/src/space_age.c
+++ c/space-age/src/space_age.c
@@ -1,16 +1,5 @@
 #include "space_age.h"
 
-const int SECS_PER_EARTH_YEAR = 31557600; 
-
-const float EARTH = 1.0; 
-const float MERCURY = 0.2408467; 
-const float VENUS = 0.61519726; 
-const float MARS = 1.8808158; 
-const float JUPITER = 11.862615; 
-const float SATURN = 29.447498; 
-const float URANUS = 84.016846; 
-const float NEPTUNE = 164.79132; 
-
 float convert_planet_age(float earth_years_per_planet_year, float age_in_secs)
 {
         float age_in_earth_years; 
--- c/space-age/src/space_age.h
+++ c/space-age/src/space_age.h
@@ -3,16 +3,16 @@
 
 #include <stdlib.h>
 
-const int SECS_PER_EARTH_YEAR; 
+#define SECS_PER_EARTH_YEAR 31557600
 
-const float EARTH; 
-const float MERCURY; 
-const float VENUS; 
-const float MARS; 
-const float JUPITER; 
-const float SATURN; 
-const float URANUS; 
-const float NEPTUNE; 
+#define EARTH 1.0
+#define MERCURY 0.2408467
+#define VENUS 0.61519726
+#define MARS 1.8808158
+#define JUPITER 11.862615
+#define SATURN 29.447498
+#define URANUS 84.016846
+#define NEPTUNE 164.79132
 
 float convert_planet_age(float earth_years_per_planet_year, float age_in_secs);
 
