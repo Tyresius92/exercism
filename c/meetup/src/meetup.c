9a3e06705dfba0d617d28ffc989b1bfb012915e2
Tyrel <tyrelclayton92@gmail.com>
2018-06-29 21:12:46 -0400

    worked on meetup

--- c/meetup/src/meetup.c
+++ c/meetup/src/meetup.c
@@ -1,13 +1,24 @@
 #include "meetup.h"
 
+time_t first_day_of_month(int year, int month)
+{
+        struct tm tm; 
+
+        tm.tm_year = year - 1900; 
+        tm.tm_mon = month - 1; 
+        tm.tm_mday = 1;
+
+        return mktime(&tm);
+}
+
 int meetup_day_of_month(int year, int month, char *which, char *day)
 {
-        (void) year; 
-        (void) month; 
         (void) which; 
         (void) day; 
 
-        
+        time_t ref_date = first_day_of_month(year, month);
+
+        fprintf(stderr, "refdate: %ld\n", ref_date);
 
         return 0; 
 }
--- c/meetup/src/meetup.h
+++ c/meetup/src/meetup.h
@@ -2,7 +2,10 @@
 #define MEETUP_H
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+
+time_t first_day_of_month(int year, int month); 
 
 int meetup_day_of_month(int year, int month, char *which, char *day); 
 
