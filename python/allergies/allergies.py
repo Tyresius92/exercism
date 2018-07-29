bedabd3d341c1616d4c95c3b7f01f639fa509419
Tyrel <tyrelclayton92@gmail.com>
2018-07-29 19:05:48 -0400

    reworked allergies based on mentor feedback

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,37 +1,32 @@
 class Allergies(object):
 
+    
+
     def __init__(self, score):
         self.score = score
+        self.allergens = {"eggs": 1,
+                          "peanuts": 2,
+                          "shellfish": 4,
+                          "strawberries": 8,
+                          "tomatoes": 16,
+                          "chocolate": 32,
+                          "pollen": 64,
+                          "cats": 128 }
+        self.allergic_to = None
 
     def is_allergic_to(self, item):
-        if item == 'eggs' and self.score & 1:
-                return True
-        elif item == 'peanuts' and self.score & 2:
+        if self.score & self.allergens[item]:
             return True
-        elif item == 'shellfish' and self.score & 4:
-            return True
-        elif item == 'strawberries' and self.score & 8:
-            return True
-        elif item == 'tomatoes' and self.score & 16:
-            return True
-        elif item == 'chocolate' and self.score & 32:
-            return True
-        elif item == 'pollen' and self.score & 64:
-            return True
-        elif item == 'cats' and self.score & 128:
-            return True
-
+        
         return False
 
     @property
     def lst(self):
-        allergens = ['eggs', 'peanuts', 'shellfish', 'strawberries',
-                     'tomatoes', 'chocolate', 'pollen', 'cats']
+        if self.allergic_to == None:
+            self.allergic_to = []
+            
+            for allergen in self.allergens.keys():
+                if self.is_allergic_to(allergen):
+                    self.allergic_to.append(allergen)
 
-        allergic_to = []
-
-        for allergen in allergens:
-            if self.is_allergic_to(allergen):
-                allergic_to.append(allergen)
-
-        return allergic_to
+        return self.allergic_to
