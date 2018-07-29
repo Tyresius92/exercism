f9e2c5e0fb7a5b7d0f44362e95068f85716a1084
Tyrel <tyrelclayton92@gmail.com>
2018-07-29 14:09:42 -0400

    updated allergies and sumofmultiples after getting mentor feedback

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,9 +1,7 @@
 def sum_of_multiples(limit, multiples):
     numbers = set()
-    
-    for i in range(1, limit):
-        for j in multiples:
-            if i % j == 0:
-                numbers.add(i)
+
+    for m in multiples: 
+        numbers.update(range(m, limit, m))
 
     return sum(numbers)
