a7fa0b0c96030c85104c22674967d508c91d94e8
Clayton <pclayt01@tufts.edu>
2018-08-06 14:31:06 -0400

    lots of stuff

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,21 +1,22 @@
+from collections import OrderedDict
+
 class Allergies(object):
-
-    allergens = {"eggs": 1,
-                 "peanuts": 2,
-                 "shellfish": 4,
-                 "strawberries": 8,
-                 "tomatoes": 16,
-                 "chocolate": 32,
-                 "pollen": 64,
-                 "cats": 128}
-
+    allergens = OrderedDict([("eggs", 1),
+                             ("peanuts", 2),
+                             ("shellfish", 4),
+                             ("strawberries", 8),
+                             ("tomatoes", 16),
+                             ("chocolate", 32),
+                             ("pollen", 64),
+                             ("cats", 128)])
+    
     def __init__(self, score):
         self.score = score
-        self._lst = [a for a in self.allergens.keys()
+        self._lst = [a for a in self.allergens
                          if self.score & self.allergens[a] != 0]
 
     def is_allergic_to(self, item):
-        return item in self._lst
+        return self.allergens[item] & self.score != 0
 
     @property
     def lst(self):
