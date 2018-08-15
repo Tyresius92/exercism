7023e3d5939a53c5606028794e137b61f107eca1
Tyrel <tyrelclayton92@gmail.com>
2018-08-14 23:32:38 -0400

    downloaded raindrops

---
+++ c/raindrops/src/raindrops.c
@@ -0,0 +1,11 @@
+#include "raindrops.h"
+
+const char *PLING = "Pling"; 
+const char *PLANG = "Plang"; 
+const char *PLONG = "Plong"; 
+
+void convert(char *result, int drops)
+{
+        (void) result; 
+        (void) drops; 
+}
