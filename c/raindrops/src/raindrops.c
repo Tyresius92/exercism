6f51e12b981931bbb97f05ed8f10d1fdeb996e99
Tyrel <tyrelclayton92@gmail.com>
2018-08-15 19:07:02 -0400

    solved raindrops

--- c/raindrops/src/raindrops.c
+++ c/raindrops/src/raindrops.c
@@ -1,11 +1,22 @@
 #include "raindrops.h"
 
-const char *PLING = "Pling"; 
-const char *PLANG = "Plang"; 
-const char *PLONG = "Plong"; 
-
 void convert(char *result, int drops)
 {
-        (void) result; 
-        (void) drops; 
+        int bytes_written = 0; 
+
+        if (drops % 3 == 0) {
+                bytes_written += sprintf(result, "Pling"); 
+        }
+
+        if (drops % 5 == 0) {
+                bytes_written += sprintf(result + bytes_written, "Plang");
+        }
+
+        if (drops % 7 == 0) {
+                bytes_written += sprintf(result + bytes_written, "Plong"); 
+        }
+
+        if (bytes_written == 0) {
+                sprintf(result, "%d", drops); 
+        }
 }
--- c/raindrops/src/raindrops.h
+++ c/raindrops/src/raindrops.h
@@ -1,6 +1,10 @@
 #ifndef RAINDROPS_H
 #define RAINDROPS_H
 
+#include <stdio.h>
+
 void convert(char *result, int drops); 
+
+char *num_to_string(int num); 
 
 #endif
