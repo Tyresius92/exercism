157e6cefb1b38b7519cebe63fb6d3595fb7232d2
Tyresius92 <tyrelclayton92@gmail.com>
2018-08-17 11:51:31 -0400

    solved kindergarten garden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,9 +1,21 @@
 class Garden(object):
+
+    plant_key = {'V': "Violets", "C": "Clover", "G": "Grass", "R": "Radishes"}
+    
     def __init__(self, diagram, students="Alice Bob Charlie David Eve "\
-                                         "Fred Ginny Harriet Ileana"\
+                                         "Fred Ginny Harriet Ileana "\
                                          "Joseph Kincaid Larry".split()):
         self.diagram = diagram.split("\n")
-        self.students = students
+        self.students = sorted(students)
 
     def plants(self, student):
-        pass
+        kid_index = self.students.index(student)
+
+        result = []
+
+        result.append(self.plant_key[self.diagram[0][kid_index * 2]])
+        result.append(self.plant_key[self.diagram[0][(kid_index * 2) + 1]])
+        result.append(self.plant_key[self.diagram[1][kid_index * 2]])
+        result.append(self.plant_key[self.diagram[1][(kid_index * 2) + 1]])
+
+        return result
