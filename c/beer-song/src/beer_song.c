fedce17ade693b267d06d9fb0bae41e00d4c6180
Tyrel <tyrelclayton92@gmail.com>
2018-08-20 15:27:29 -0400

    refactored based on mentor feedback

--- c/beer-song/src/beer_song.c
+++ c/beer-song/src/beer_song.c
@@ -1,30 +1,35 @@
 #include "beer_song.h"
 
+const size_t MAX_BUFF = 1024;
+
 void verse(char *buffer, int verse_num)
 {
         if (verse_num > 99 || verse_num < 0)
                 return; 
 
+        memset(buffer, '\0', MAX_BUFF); 
+
+
         if (verse_num > 2) {
-                sprintf(buffer, "%d bottles of beer on the wall, " \
+                snprintf(buffer, MAX_BUFF, "%d bottles of beer on the wall, " \
                         "%d bottles of beer.\n" \
                         "Take one down and pass it around, " \
                         "%d bottles of beer on the wall.\n", 
                         verse_num, verse_num, verse_num - 1);
         } else if (verse_num == 2) {
-                sprintf(buffer, "%d bottles of beer on the wall, " \
+                snprintf(buffer, MAX_BUFF, "%d bottles of beer on the wall, " \
                         "%d bottles of beer.\n" \
                         "Take one down and pass it around, " \
                         "%d bottle of beer on the wall.\n", 
                         verse_num, verse_num, verse_num - 1);
         } else if (verse_num == 1) {
-                sprintf(buffer, "%d bottle of beer on the wall, " \
+                snprintf(buffer, MAX_BUFF, "%d bottle of beer on the wall, " \
                         "%d bottle of beer.\n" \
                         "Take it down and pass it around, " \
                         "no more bottles of beer on the wall.\n", 
                         verse_num, verse_num);
         } else {  // verse_num == 0
-                sprintf(buffer, "No more bottles of beer on the wall, " \
+                snprintf(buffer, MAX_BUFF, "No more bottles of beer on the wall, " \
                         "no more bottles of beer.\n" \
                         "Go to the store and buy some more, " \
                         "99 bottles of beer on the wall.\n");
@@ -35,18 +40,20 @@ void verse(char *buffer, int verse_num)
 
 void sing(char *buffer, int start_verse, int end_verse)
 {
-        memset(buffer, '\0', 1024); 
-
         if (start_verse < end_verse)
                 return; 
 
-        char local_buff[1024]; 
+        memset(buffer, '\0', MAX_BUFF); 
+
+        char local_buff[MAX_BUFF]; 
+        memset(local_buff, '\0', MAX_BUFF); 
+
 
         for (int i = start_verse; i >= end_verse; i--) {
                 verse(local_buff, i); 
 
-                strcat(buffer, local_buff); 
+                strncat(buffer, local_buff, MAX_BUFF); 
                 if (i != end_verse) 
-                        strcat(buffer, "\n"); 
+                        strncat(buffer, "\n", MAX_BUFF); 
         }
 }
--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,21 +1,24 @@
 class Garden(object):
 
     plant_key = {'V': "Violets", "C": "Clover", "G": "Grass", "R": "Radishes"}
+
+    default_kids = "Alice Bob Charlie David Eve Fred Ginny "\
+                   "Harriet Ileana Joseph Kincaid Larry".split()
     
-    def __init__(self, diagram, students="Alice Bob Charlie David Eve "\
-                                         "Fred Ginny Harriet Ileana "\
-                                         "Joseph Kincaid Larry".split()):
+    def __init__(self, diagram, students=None):
         self.diagram = diagram.split("\n")
+        
+        if students == None:
+            students = self.default_kids
         self.students = sorted(students)
 
     def plants(self, student):
-        kid_index = self.students.index(student)
+        kid_index = self.students.index(student) * 2
 
-        result = []
+        return [
+            self.plant_key[self.diagram[0][kid_index]],
+            self.plant_key[self.diagram[0][kid_index + 1]],
+            self.plant_key[self.diagram[1][kid_index]],
+            self.plant_key[self.diagram[1][kid_index + 1]]
+            ]
 
-        result.append(self.plant_key[self.diagram[0][kid_index * 2]])
-        result.append(self.plant_key[self.diagram[0][(kid_index * 2) + 1]])
-        result.append(self.plant_key[self.diagram[1][kid_index * 2]])
-        result.append(self.plant_key[self.diagram[1][(kid_index * 2) + 1]])
-
-        return result
