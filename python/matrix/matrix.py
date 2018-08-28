360e1c96b1f2856e05c2de59e1a8851930b8e4ea
Tyrel <tyrelclayton92@gmail.com>
2018-08-28 00:09:23 -0400

    solved matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,15 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        split_matrix = matrix_string.split('\n')
+        for i in range(len(split_matrix)):
+            split_matrix[i] = split_matrix[i].split()
+            for j in range(len(split_matrix[i])):
+                split_matrix[i][j] = int(split_matrix[i][j])
+
+        self.matrix = split_matrix
 
     def row(self, index):
-        pass
+        return self.matrix[index]
 
     def column(self, index):
-        pass
+        return [self.matrix[i][index] for i in range(len(self.matrix))]
