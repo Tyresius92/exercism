b23f0fa7d74218b1a3c970e53eea7a95638d3036
Tyresius92 <tyrelclayton92@gmail.com>
2018-09-04 17:07:28 -0400

    solved minesweeper

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,19 +1,33 @@
-def board(input_board_array):
-    for i in range(len(input_board_array)):
-        for j in range(len(input_board_array[i])):
-            if input_board_array[i][j] == '*':
+def board(ray):
+    for i in range(len(ray)):
+        
+        if len(ray[i]) != len(ray[0]):
+            raise ValueError('Invalid Board!')
+        
+        for j in range(len(ray[i])):
+            if ray[i][j] == '*':
                 continue
             else:
-                num = count_mines(input_board_array, i, j)
-                input_board_array[i] = input_board_array[i][:j] + str(num) + input_board_array[i][j:]
+                num = count_mines(ray, i, j)
+                if num != 0: 
+                    ray[i] = ray[i][:j] + str(num) + ray[i][j + 1:]
 
-def count_mines(in_ray, i, j):
+    return ray
+
+def count_mines(ray, i, j):
     count = 0
+    
     for x in range(i - 1, i + 2):
         for y in range(j - 1, j + 2):
-            if x <= 0 or y <= 0 or x >= len(in_ray) or y >= len(in_ray[i]):
+            
+            if x < 0 or y < 0 or x >= len(ray) or y >= len(ray[i]):
                 continue
-            if in_ray[x][y] == '*':
+            
+            if ray[x][y] == '*':
                 count += 1
+            elif ray[x][y] == ' ' or str.isdigit(ray[x][y]):
+                continue
+            else:
+                raise ValueError('Invalid Board!')
 
     return count
