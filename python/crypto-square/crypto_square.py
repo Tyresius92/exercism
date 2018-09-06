dd8564a471e268cb955c915a6eee78f419c942a5
Tyresius92 <tyrelclayton92@gmail.com>
2018-09-06 16:56:54 -0400

    first attempt at crypto square. need to switch to rows and cols

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,2 +1,29 @@
+from math import sqrt
+
 def encode(plain_text):
-    pass
+    result = ''
+    stripped = normalize_text(plain_text)
+    root = int(sqrt(len(stripped)))
+
+    if root == 0:
+        return result
+
+    if root * root < len(stripped):
+        root = root + 1
+        print(root)
+        while len(stripped) < (root)**2:
+            stripped = stripped + ' '
+    print(stripped + '.')
+
+    for i in range(root):
+        if i != 0:
+            result = result + ' '
+        for j in range(i, len(stripped), root):
+            result = result + stripped[j]
+            print(result)
+
+    return result
+
+def normalize_text(plain_text):
+    result = ''.join(c for c in plain_text.lower() if c.isalnum())
+    return result
