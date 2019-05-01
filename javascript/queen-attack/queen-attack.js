dfcafea261020a3b345e0a15b9c7f7ace096cba0
Tyrel <tyrelclayton92@gmail.com>
2019-04-30 20:25:10 -0500

    solved queen attack

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -1,6 +1,42 @@
-var Queens = function () {
-        
-};
+class Queens {
+  constructor(obj = { white: [0, 3], black: [7, 3] }) {
+    if (obj.white[0] === obj.black[0] && obj.white[1] === obj.black[1]) {
+      throw "Queens cannot share the same space";
+    }
 
+    this.white = obj.white;
+    this.black = obj.black;
+  }
 
-module.exports = Queens; 
+  toString() {
+    let board = [];
+
+    for (let i = 0; i < 8; i++) {
+      let row = [];
+      for (let j = 0; j < 8; j++) {
+        if (i === this.white[0] && j === this.white[1]) {
+          row.push("W");
+        } else if (i === this.black[0] && j === this.black[1]) {
+          row.push("B");
+        } else {
+          row.push("_");
+        }
+      }
+      row = row.join(" ");
+      board.push(row);
+    }
+
+    return board.join("\n") + "\n";
+  }
+
+  canAttack() {
+    return (
+      this.white[0] === this.black[0] ||
+      this.white[1] === this.black[1] ||
+      Math.abs(this.white[0] - this.black[0]) ===
+        Math.abs(this.white[1] - this.black[1])
+    );
+  }
+}
+
+export default Queens;
